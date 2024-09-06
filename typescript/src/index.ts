3d050b0d79b29c29143e1006224a1d88bf1fba1e
Yuna Shin <yoonahs@andrew.cmu.edu>
2024-09-06 12:49:07 -0400

    Add interface and change Rectangle to Shape

--- typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
-
-const rectangle: Rectangle = newRectangle(2, 3)
-const renderer = newRenderer(rectangle)
-renderer.draw();
